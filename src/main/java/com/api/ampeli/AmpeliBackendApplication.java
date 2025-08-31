d4c0deb91526c464a2ee469ff903ea1e5f66c6fe
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2025-08-31 00:27:00 -0300

    start projeto

---
+++ src/main/java/com/api/ampeli/AmpeliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.ampeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpeliBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmpeliBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/ampeli/AmpeliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ampeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpeliBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
